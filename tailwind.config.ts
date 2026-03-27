4927525c50027cca62c8407082a94aa08a244004
Margarita Lizardo <maggielizardo@Maggie.local>
2026-03-27 08:55:45 +0800

    eval side

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                alviona: ['Alviona', 'serif'],
+            },
+        },
+    },
+    plugins: [],
+};
